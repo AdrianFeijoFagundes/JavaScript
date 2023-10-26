0ee8b20d3938972e1c8b93869643930b3bf3a1a6
Adrian Fagundes <adrianfeijofagundes@gmail.com>
2023-10-25 21:55:33 -0300

    operadores pt2

---
+++ javascript-básico/operadores/operadores-logicos.js
@@ -0,0 +1,18 @@
+// &&   conjunção       e 
+// ||   disjunção       ou
+// !    negação         não
+console.log('Operador !')
+console.log('!true: ' +(!true))
+console.log('!false: ' +(!false))
+
+console.log('Operador &&')
+console.log('false && false: ' +(false && false))
+console.log('false && true: ' +(false && true))
+console.log('true && false: ' +(true && false))
+console.log('true && true: ' +(true && true))
+
+console.log('Operador ||')
+console.log('false || false: ' + (false || false))
+console.log('false || true: ' + (false || true))
+console.log('true || false: ' +(true || false))
+console.log('true || true: ' +(true || true))
