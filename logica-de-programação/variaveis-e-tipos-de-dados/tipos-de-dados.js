3b253ab79bb144e82e62ce0d1b6a48cd6bb95135
Adrian Fagundes <adrianfeijofagundes@gmail.com>
2023-11-04 10:08:46 -0300

    tipos de dados

--- logica-de-programação/variaveis-e-tipos-de-dados/tipos-de-dados.js
+++ logica-de-programação/variaveis-e-tipos-de-dados/tipos-de-dados.js
@@ -11,3 +11,14 @@ let obj = {};                       // object
 console.log(
     `\n ${typeof str} \n ${typeof num} \n ${typeof bol} \n ${n} \n ${typeof un} \n ${typeof fuc} \n ${typeof obj} `
 );
+
+// O comando typeof mostra o tipo de dado seja em uma váriavel ou não
+// Exemplo:
+console.log('Exemplo de outro uso de typeof')
+console.log(typeof 202)             // number
+console.log(typeof (200 == 100))    //boolean
+console.log(typeof (() => {}))      //function
+console.log(typeof [])              //object
+console.log(typeof {})              //object
+console.log(typeof null)            //object
+console.log(typeof '')              //string
